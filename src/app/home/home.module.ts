110f6ea8dfe33b293e40c63e2ee2602d49d62853
Mangesh55 <mdatar@mobiquityinc.com>
2020-01-04 08:35:06 +0530

    Completed Login flow using firebase.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
+    AngularFireAuthModule,
     RouterModule.forChild([
       {
         path: '',
@@ -18,6 +20,8 @@ import { HomePage } from './home.page';
       }
     ])
   ],
+  exports : [ FormsModule,
+    ReactiveFormsModule,AngularFireAuthModule],
   declarations: [HomePage]
 })
 export class HomePageModule {}
